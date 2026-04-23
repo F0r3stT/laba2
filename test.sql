bdf34eaf03eb01c2c0d1a5ebf2843af4cde63508
VoLDeMaR729 <atonvladimir7@gmail.com>
2026-04-23 19:48:25 +0700

    Update test.sql

--- test.sql
+++ test.sql
@@ -22,8 +22,8 @@ INSERT INTO discovery_source (source_name, source_type) VALUES
 ('MaxPatrol', 'Автоматический скан'), 
 ('Bug Bounty', 'Ручной поиск');
 
--- 5. Заполняем сотрудников
-INSERT INTO employee (full_name, job_title, email, id_department) VALUES 
+-- 5. Заполняем сотрудников 
+INSERT INTO employee (full_name, position, email, id_department) VALUES 
 ('Иванов Иван', 'Специалист ИБ', 'ivanov@it.ru', 1),
 ('Петров Петр', 'Системный администратор', 'petrov@it.ru', 2),
 ('Смирнов Алексей', 'Аналитик SOC', 'smirnov@it.ru', 1);
@@ -34,14 +34,14 @@ INSERT INTO asset (asset_name, ip_address, asset_type, id_department) VALUES
 ('Почтовый сервер', '192.168.1.20', 'Сервер', 2),
 ('Рабочая станция CEO', '192.168.2.5', 'ПК', 1);
 
--- 7. Заполняем уязвимости
-INSERT INTO vulnerability (id_cve, description, id_status, id_asset, id_severity, id_source) VALUES 
-('CVE-2026-0001', 'SQL-инъекция в панели входа', 1, 1, 4, 3), -- Критическая на БД
-('CVE-2026-0002', 'Устаревшая версия Nginx', 2, 2, 2, 1),    -- Средняя на почте
-('CVE-2026-0003', 'Слабый пароль RDP', 1, 1, 3, 2),          -- Высокая на БД
-('CVE-2026-0004', 'Открытый порт 445', 1, 3, 2, 1);          -- Средняя на ПК
+-- 7. Заполняем уязвимости 
+INSERT INTO vulnerability (cve_id, description, id_status, id_asset, id_severity, id_source) VALUES 
+('CVE-2026-0001', 'SQL-инъекция в панели входа', 1, 1, 4, 3),
+('CVE-2026-0002', 'Устаревшая версия Nginx', 2, 2, 2, 1),
+('CVE-2026-0003', 'Слабый пароль RDP', 1, 1, 3, 2),
+('CVE-2026-0004', 'Открытый порт 445', 1, 3, 2, 1);
 
--- 8. Заполняем инциденты (даты ставим апрель 2026 для твоего 28 запроса)
+-- 8. Заполняем инциденты
 INSERT INTO incident (detected_at, description, id_status, id_vulnerability, id_employee) VALUES 
 ('2026-04-10 10:00:00', 'Попытка эксплуатации SQLi', 2, 1, 1),
 ('2026-04-15 12:30:00', 'Сканирование портов', 3, 3, 1),
